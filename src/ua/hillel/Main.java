a47fce59456eb597d6858ebac96d2afbfb0836af
Denys <den333350@gmail.com>
2021-07-19 13:46:53 +0300

    MVP

--- src/ua/hillel/Main.java
+++ src/ua/hillel/Main.java
@@ -34,52 +34,78 @@ class Menu{
     ArrayList<Task> completedTask = new ArrayList<>();
     Scanner s = new Scanner(System.in);
 
+    public void showTask(){
+        System.out.println("List of uncompleted task:");
+        if(uncompletedTask.isEmpty()) System.out.println("Empty");
+        else {
+            for (int i = 0; i < uncompletedTask.size() ; i++) {
+                uncompletedTask.get(i).print(i+1);
+            }
+        }
+    }
 
 
     public void start(){
-
-        while(true){
+        showTask();
+        menu :while(true){
             System.out.println("1-Create new task\t 2-Make task completed\t 3-Show completed task\t 4-Show all task\t 0-Exit");
-
-
             switch (s.nextLine()){
                 case("1"): //Create new task
                     System.out.print("Write description: ");
                     String task = s.nextLine();
                     Task newTask = new Task(task);
                     uncompletedTask.add(newTask);
+                    showTask();
                     break;
+
+
                 case("2"): //Make task completed
                     System.out.print("Choose task: ");
                     int choose = s.nextInt()-1;
                     uncompletedTask.get(choose).makeCompleted();
                     completedTask.add(uncompletedTask.get(choose));
                     uncompletedTask.remove(choose);
-                    break;
-                case("3"): //Show completed task
-                    for (int i = 0; i < completedTask.size() ; i++) {
-                        completedTask.get(i).print(i+1);
-                    }
-                    break;
-                case("4"): //Show all task
-                    for (int i = 0; i < uncompletedTask.size() ; i++) {
-                        uncompletedTask.get(i).print(i+1);
-                    }
-                    for (int i = 0; i < completedTask.size() ; i++) {
-                        completedTask.get(i).print(i+1);
-                    }
-                    break;
-                case("0"): //Exit
+                    showTask();
                     break;
 
+
+                case("3"): //Show completed task
+                    while (true) {
+                        System.out.println("List of uncompleted task:");
+                        if (completedTask.isEmpty()) System.out.println("Empty");
+                        else {
+                            for (int i = 0; i < completedTask.size(); i++) {
+                                completedTask.get(i).print(i + 1);
+                            }
+                        }
+                        System.out.println("1-Make task uncompleted\t0-Back");
+                        switch (s.nextLine()) {
+                            case ("1"):
+                                System.out.print("Choose task: ");
+                                int choose1 = s.nextInt() - 1;
+                                completedTask.get(choose1).makeUncompleted();
+                                uncompletedTask.add(completedTask.get(choose1));
+                                completedTask.remove(choose1);
+                                break;
+                            case ("0"):
+                                continue menu;
+                        }
+                    }
+
+                case("4"): //Show all task
+
+                    for (int i = 0; i < completedTask.size() ; i++) {
+                        completedTask.get(i).print(i+1);
+                    }
+                    showTask();
+                    break;
+
+                case("0"): //Exit
+                    System.exit(0);
+
+                    break;
             }
-            System.out.println("List of uncompleted task:");
-            if(uncompletedTask.isEmpty()) System.out.println("Empty");
-            else {
-                for (int i = 0; i < uncompletedTask.size() ; i++) {
-                    uncompletedTask.get(i).print(i+1);
-                }
-            }
+
         }
 
     }
